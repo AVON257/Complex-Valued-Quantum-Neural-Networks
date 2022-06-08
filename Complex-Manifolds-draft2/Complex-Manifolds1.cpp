c52986025db82705b4029a4aa01a664c22d014d1
Thomas N Toseland <tntavon27@gmail.com>
2022-06-08 23:39:17 +0100

    Add files via upload
    Note that these are just prototypes and that this is not yet finished. At the moment this only does partial coordinate conversion between cartesian and Toroidal coordinates, but when finished it will be capable of doing that in both directions, and of calculating derivatives/integrals as mentioned in the README file.

---
+++ Complex-Manifolds-draft2/Complex-Manifolds1.cpp
@@ -0,0 +1,30 @@
+//============================================================================
+// Name        : Complex-Manifolds1.cpp
+// Author      : T N TOSELAND
+// Version     :
+// Copyright   : Your copyright notice
+// Description : This is a prototype machine learning loop for use with Bayesian Neural Networks, using several complex variables theory.
+// Note that this can actually be adapted for use in other areas, such as numerical analysis.
+// Note also that this does not produce a closed form derivative output.
+// This file is primarily for the user interface.
+//============================================================================
+
+#include <iostream>
+#include "Torus.hxx"
+
+// Still to do on this: Implement the derivative and integral loops, Torus.hxx contains the required variables and coordinate transforms from cartesian.
+
+using namespace std;
+
+int main() {
+	cout << "Please enter x coordinate" << endl; // prints !!!Hello World!!!
+	Toroid::cartesian::cartd1(); // This needs an input to be useful.
+	{
+
+class Derivative {
+public:
+
+};
+	}
+	return 0;
+}
